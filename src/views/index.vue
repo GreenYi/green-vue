2bcabb44c3a7f535fdad393fedb722c59a8672aa
greenyi <green.gm.yi@foxmail.com>
2022-10-10 09:49:38 +0800

    删除无用注释代码

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,20 +1,5 @@
 <template>
   <div>
-    <!-- <el-main router class="el-menu-vertical-demo">
-      <el-carousel :interval="4000" type="card" height="300PX">
-        <template v-for="route in $router.options.routes">
-          <el-carousel-item
-            v-for="item in route.children"
-            :key="route.path + '/' + item.path"
-            :index="item.path"
-            @click.native="linkTo(item.path)"
-          >
-            <i :class="item.icon"></i>
-            <h3 class="medium">{{ item.name }}</h3>
-          </el-carousel-item>
-        </template>
-      </el-carousel>
-    </el-main> -->
   </div>
 </template>
 <style>
